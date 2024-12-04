f9498bdf7cb3f0c49e458cd2598cca66cae6649c
Kirill Shklyaev <kirill@anytype.io>
2024-12-04 22:55:25 +0300

    del symbol / from tests

--- tests/run.d/setExternalListenHosts.sh
+++ tests/run.d/setExternalListenHosts.sh
@@ -1,4 +1,4 @@
 # Sets multiple external listen hosts in .env.override
 cat <<EOF > $PROJECT_DIR/.env.override
-EXTERNAL_LISTEN_HOSTS=\"$EXTERNAL_LISTEN_HOSTS\"
+EXTERNAL_LISTEN_HOSTS="$EXTERNAL_LISTEN_HOSTS"
 EOF
